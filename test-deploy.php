ab2ca4103d104cd8e16466cef229bb39da110a79
Fariz Hermawan <fariz.hermawan@traveloka.com>
2019-11-05 01:16:53 +0700

    Test deploy via cli

--- test-deploy.php
+++ test-deploy.php
@@ -1,31 +1,38 @@
 <?php
-ob_end_flush();
-ob_implicit_flush();
-
-$project = $_GET['project'];
 $start = time();
 
-echo "<pre>";
+if (PHP_SAPI === 'cli') {
+  $project = $_SERVER['argv'][1];
+} else {
+  ob_end_flush();
+  ob_implicit_flush();
+  $project = $_GET['project'];
+  echo "<pre>";
+}
+
 if (!$project) echo "Missing required parameter `project` (e.g: riska-data-be)";
 
 echo "~ cd {$project}\n\n";
-chdir("/var/www/html/{$project}");
+chdir("../{$project}");
 
 $cmd_after_pull = substr($project, -2, 2) == "fe"
   ? "npm install && bower install"
-  : "composer install";
+  : "composer install && php artisan migrate";
 
 $cmds = ['git reset --hard HEAD', 'git pull'];
 foreach (explode(" && ", $cmd_after_pull) as $cmd) $cmds[] = $cmd;
 
 foreach ($cmds as $cmd) {
-  $output = [];
   echo "~ " . $cmd . "\n";
-  exec($cmd . " 2>&1", $output, $exit);
-  echo implode("\n", $output);
-  echo "\n\n";
+  if( ($fp = popen($cmd . " 2>&1", "r")) ) {
+    while( !feof($fp) ){
+      echo fread($fp, 1024);
+      flush();
+    }
+    fclose($fp);
+  }
 }
 
 $end = time();
 
-echo "finish in " . ($end - $start) . "ms";
+echo "finished in " . ($end - $start) . "ms\n";
